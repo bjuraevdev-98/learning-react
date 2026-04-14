f6b63cc076dee35106a2b2579796cf138b2135d5
Bekzod Juraev <bekzodjuraev.dev@gmail.com>
2026-04-14 15:08:14 -0700

    Proper React Setup

---
+++ chabot-project/src/components/ChatInput.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { Chatbot } from 'supersimpledev';
+import LoadingSpinner from '../assets/loading-spinner.gif';
+import './ChatInput.css'
+
+export function ChatInput({ chatMessages, setChatMessages }) {
+  const [inputText, setInputText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  function saveInputText(event) {
+      setInputText(event.target.value)
+  }
+
+  function keyPressed(event) {
+      if (!inputText || isLoading) return;
+      if (event.key === 'Enter') {
+          sendMessage();
+          setInputText('');
+      }
+      if (event.key === 'Escape') {
+          setInputText('');
+      }
+  }
+
+  async function sendMessage() {
+      if (!inputText || isLoading) return;
+      setInputText('');
+
+      const newChatMessages = [
+          ...chatMessages,
+          {
+              message: inputText,
+              sender: 'user',
+              id: crypto.randomUUID()
+          },
+          {
+              message: <img src={LoadingSpinner} className="loading-spinner" />,
+              sender: 'robot',
+              id: crypto.randomUUID()
+          }
+      ];
+
+      setChatMessages(newChatMessages);
+
+      setIsLoading(true);
+
+      const response = await Chatbot.getResponseAsync(inputText);
+      setChatMessages([
+          ...newChatMessages.slice(0, newChatMessages.length - 1),
+          {
+              message: response,
+              sender: 'robot',
+              id: crypto.randomUUID()
+          }
+      ]);
+
+      setIsLoading(false);
+  }
+
+  return (
+      <div className="chat-input-container">
+          <input 
+              placeholder="Send a message to Chatbot" 
+              size="30" 
+              onChange={saveInputText}
+              value={inputText}
+              onKeyDown={keyPressed}
+              className="chat-input"
+          />
+          <button
+              onClick={sendMessage}
+              className="send-button"
+          >Send</button>
+      </div>
+  );
+}
---
+++ chabot-project/src/components/ChatMessages.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef } from 'react';
+import { ChatMessage } from './ChatMessage';
+import './ChatMessages.css'
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useAutoScroll([chatMessages]);
+  
+  if (chatMessages.length === 0) {
+      return (
+          <p className='welcome-text'>
+              Welcome to the chatbot project! Send a message using the textbox below.
+          </p>
+      )
+  }
+
+  return (
+      <div 
+          className="chat-messages-container"
+          ref={chatMessagesRef}
+      >
+          { chatMessages.map(({ message, sender, id }) => {
+          return (
+              <ChatMessage 
+                  message={ message }
+                  sender={ sender }
+                  key={ id }
+              />
+          )
+          })}
+      </div>
+  )
+}
+
+export default ChatMessages;
+
+function useAutoScroll(dependencies) {
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        const containerElem = containerRef.current;
+        if (containerElem) {
+            containerElem.scrollTop = containerElem.scrollHeight;
+        }
+    }, [dependencies]);
+
+    return containerRef;
+}
