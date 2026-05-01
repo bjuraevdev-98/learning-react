07316c8e735270b41a80f7a106849c83a4b2b8f0
Bekzod Juraev <bekzodjuraev.dev@gmail.com>
2026-04-30 21:04:18 -0700

    added a test for selecting a quantity

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -66,5 +66,13 @@ describe('Product component', () => {
         );
 
         expect(loadCart).toHaveBeenCalled();
-    })
+    });
+
+    it('selects a quantity', () => {
+        render(<Product product={product} loadCart={loadCart} />);
+
+        const quantitySelector = screen.getByTestId('product-quantity-selector');
+
+        expect(quantitySelector).toHaveValue('1');
+    });
 })
